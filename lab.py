53ea5c1773bbe9c221520d9bdd32342cf03122c3
오곡밥튀김 <38698459+kokbab88611@users.noreply.github.com>
2023-06-26 04:13:44 +0900

    Update lab.py

--- lab.py
+++
@@ -1,3 +0,0 @@
-import os 
-os.environ["Bin_API_KEY"] = "q4VWoBc1oXblQ32ORbrDIb0O2vnXczXYqwR35EtGeF8r8uAx41DJ7HgjExioAVcE"
-os.environ["Bin_SECRET_KEY"] = "RSWuB5Ico0XlZAH39C27Z3KSgepcNRYKPxpucvDhnxhPnCfaCt1Jz3bMM2vCu78L"
